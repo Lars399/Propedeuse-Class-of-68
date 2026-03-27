fc150f13e6f20702a02f46c4695ea87042203b31
iuliabac <baca0003@hz.nl>
2026-03-27 12:36:44 +0100

    deleted the fire button

--- code/front-end/FireButton.ts
+++
@@ -1,8 +0,0 @@
-<button id="fireAlertButton" style="position: absolute; top: 0; left: 0; padding: 10px 20px; background-color: red; color: white; border: none; cursor: pointer;">Trigger Fire Alert</button>
-
-<script>
-    document.getElementById('fireAlertButton').addEventListener('click', function() {
-        const fireID = Math.floor(Math.random() * 10000);
-        alert('Fire Alert Triggered! Fire ID: ' + fireID);
-    });
-</script>
