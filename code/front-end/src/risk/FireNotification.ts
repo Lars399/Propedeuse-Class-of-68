797911a2edf9442bd86b0ff435e6dd21e3368d90
Lars Loois <235134@student.scalda.nl>
2026-03-27 15:08:44 +0100

    alerts popping up

--- code/front-end/src/risk/FireNotification.ts
+++ code/front-end/src/risk/FireNotification.ts
@@ -23,9 +23,24 @@ export default class FireNotification {
         setTimeout(() => {
             notification.remove();
         }, 5000);
+
+        this.notificationContainer.style.position = 'fixed';
+        this.notificationContainer.style.top = '20px';
+        this.notificationContainer.style.right = '20px';
+        this.notificationContainer.style.width = '250px';
+        this.notificationContainer.style.zIndex = '9999';
+        notification.style.backgroundColor = '#ff4d4d';
+        notification.style.color = 'white';
+        notification.style.padding = '10px 15px';
+        notification.style.marginBottom = '10px';
+        notification.style.borderRadius = '5px';
+        notification.style.boxShadow = '0 2px 6px rgba(0,0,0,0.3)';
     }
+
+    
 }
 
 // Usage Example:
 const fireNotification = new FireNotification();
-fireNotification.showNotification('Fire alert! Evacuate immediately!');
+fireNotification.showNotification('Test alert!');
+
