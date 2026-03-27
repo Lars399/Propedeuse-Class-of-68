bb9f6c214b223f0757449867bde6f88432b09703
Iulia T. <baca0003@hz.nl>
2026-03-27 11:50:26 +0100

    Add FireDepartment class to eliminate fires at specific coordinates

---
+++ code/back-end/FireDepartment.ts
@@ -0,0 +1,8 @@
+class FireDepartment {
+    static eliminateFire(x: number, y: number): void {
+        console.log(`Eliminating fire at coordinates (${x}, ${y})`);
+        // Additional logic to eliminate fire can be implemented here.
+    }
+}
+
+export default FireDepartment;
